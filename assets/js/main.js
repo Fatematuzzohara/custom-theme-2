9ac2152f6313575c87001e1c1a40feda3258597d
Fatematuzzohara <siliakhan0@gmail.com>
2023-02-01 13:44:44 +0600

    1st feb

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,21 +63,7 @@
   /**
    * Scrolls to an element with header offset
    */
-  const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
-
-    if (!header.classList.contains('header-scrolled')) {
-      offset -= 10
-    }
-
-    let elementPos = select(el).offsetTop
-    window.scrollTo({
-      top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
-
+ 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
